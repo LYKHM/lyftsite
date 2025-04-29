2e246c74272daf432a5c7595f7e8383c52043ce3
LYKHM <lyckholm1@icloud.com>
2025-04-29 12:14:00 +0200

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Policy from './policy.jsx'
-import Terms from './terms.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
