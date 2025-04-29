35089f66d8116df82fb63433557dbf1d8e06498c
LYKHM <lyckholm1@icloud.com>
2025-04-29 18:24:41 +0200

    new fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Policy from './Policy';
+import Policy from './policy';
 import Terms from './terms';
 import Home from './home';
 
